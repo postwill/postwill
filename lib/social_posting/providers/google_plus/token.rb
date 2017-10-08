53e9e1369ba40a9be57ae245dec697aafe9bbb7b
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2017-10-08 09:48:07 +0300

    Remove oauth http request

--- lib/social_posting/providers/google_plus/token.rb
+++ lib/social_posting/providers/google_plus/token.rb
@@ -2,22 +2,28 @@ module SocialPosting
   module Providers
     module GooglePlus
       class Token
+        TOKEN_CREDENTIALS_URI = 'https://www.googleapis.com/oauth2/v3/token'
         OAUTH_URI = URI('https://accounts.google.com/o/oauth2/token')
 
-        attr_accessor :access_token, :expires_at, :id_token, :expires_in, :token_type
-        attr_reader :refresh_token, :client_id, :client_secret, :callback_url
+        attr_reader :refresh_token, :client_id, :client_secret, :scope
 
         def initialize(options)
           @refresh_token = options[:refresh_token]
           @client_id = options[:client_id]
           @client_secret = options[:client_secret]
-          @callback_url = options[:callback_url]
+          @scope = parse_scope(options[:scope])
 
-          refresh!
+          authorization.fetch_access_token!
         end
 
         def authorization
-          Signet::OAuth2::Client.new(credentials)
+          @authorization ||= Signet::OAuth2::Client.new(
+            scope: scope,
+            refresh_token: refresh_token,
+            client_id: client_id,
+            client_secret: client_secret,
+            token_credential_uri: TOKEN_CREDENTIALS_URI
+          )
         end
 
         def credentials
@@ -25,22 +31,16 @@ module SocialPosting
             refresh_token: refresh_token,
             client_id:     client_id,
             client_secret: client_secret,
-            access_token:  access_token,
-            expires_in:    expires_in,
-            id_token:      id_token,
-            token_type:    token_type
+            access_token:  authorization.access_token,
+            expires_in:    authorization.expires_in,
+            id_token:      authorization.id_token
           }
         end
 
         private
 
-        def refresh!
-          request = Net::HTTP.post_form(OAUTH_URI, credentials.merge(grant_type: 'refresh_token'))
-          data = JSON.parse(request.body)
-          self.access_token = data['access_token']
-          self.expires_in = data['expires_in']
-          self.id_token = data['id_token']
-          self.token_type = data['token_type']
+        def parse_scope(*scopes)
+          scopes.flatten.map { |scope| "https://www.googleapis.com/auth/#{scope}" }
         end
       end
     end
